89cae3052e68b9094edc34d1f9f2862ec468e7b4
konstantin0s <constantintofan85@gmail.com>
2018-01-13 21:28:18 +0100

    Use letter row

---
+++ src/LettersRow.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import { PropTypes } from 'prop-types';
+import './LettersRow.css';
+
+class LettersRow extends Component {
+  render() {
+    return (
+    <div className="LettersRow">
+      {this.props.children}
+    </div>
+    );
+  }
+}
+
+LettersRow.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.element).isRequired,
+}
+
+
+export default LettersRow;
