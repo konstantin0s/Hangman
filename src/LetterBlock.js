8a1678ad79170f35eba9f86c92d7ca98e56683ee
konstantin0s <constantintofan85@gmail.com>
2018-01-13 21:27:31 +0100

    Add block letter js and css

---
+++ src/LetterBlock.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import { PropTypes } from 'prop-types';
+import './LetterBlock.css';
+
+class LetterBlock extends Component {
+  render() {
+    return (
+      <div onClick={this.props.onClick} className="LetterBlock">
+        <span>
+          {this.props.value}
+        </span>
+      </div>
+    );
+  }
+}
+
+LetterBlock.propTypes = {
+  value: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
+
+export default LetterBlock;
