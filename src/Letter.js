74f988dfb82fb4b833d2ec1097dde59fe3651c16
konstantin0s <constantintofan85@gmail.com>
2018-01-13 21:26:38 +0100

    Add Letter

---
+++ src/Letter.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react';
+
+import { PropTypes } from 'prop-types';
+
+class Letter extends Component {
+  render() {
+    return (
+      <span className="Letter">
+        {this.props.value}
+      </span>
+    );
+  }
+}
+
+Letter.propTypes = {
+  value: PropTypes.string.isRequired,
+};
+
+export default Letter
