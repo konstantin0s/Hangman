b8eff969e58fca2e52f6325116f0869801819a5f
konstantin0s <constantintofan85@gmail.com>
2018-01-13 21:29:09 +0100

    Add restart button for game over

---
+++ src/RestartButton.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import { PropTypes } from 'prop-types';
+
+import { GAME_WON } from './game-states';
+
+import './RestartButton.css';
+
+class RestartButton extends Component {
+  render() {
+    return (
+      <div className="App-Restart">
+        <button onClick={this.props.onClick}>
+          {this.props.gameState === GAME_WON ? 'Play' : 'Try'} again
+        </button>
+      </div>
+    );
+  }
+}
+
+RestartButton.propTypes = {
+  gameState: PropTypes.symbol.isRequired,
+  onClick: PropTypes.func.isRequired,
+}
+
+export default RestartButton;
