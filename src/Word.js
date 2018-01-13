4001241dc95f889cf9d4b84571614d963ab08720
konstantin0s <constantintofan85@gmail.com>
2018-01-13 21:30:03 +0100

    Add list of randomized words to the game

---
+++ src/Word.js
@@ -0,0 +1,21 @@
+
+import React, { Component } from 'react';
+
+import { PropTypes } from 'prop-types';
+import './Word.css';
+
+class Word extends Component {
+  render() {
+    return (
+      <div className="Word">
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+Word.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.element),
+}
+
+export default Word;
